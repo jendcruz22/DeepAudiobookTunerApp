66158ec53b6e05b16b4c711796c5f65501bff10e
jendcruz22 <jendcruz23@gmail.com>
2021-03-22 14:06:01 +0530

    removed : player preloader

--- static/js/playerpreloader.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    setTimeout(function(){
-     $("#preloader").delay(650).removeClass("animation").addClass("over");
-     $(".pre-overlay").css({"height" : "0%"});
-    }, 4000);
-   });
