711eaa6c90c9515b5823465240ea0806f48eab03
Daniel Lobo <danlobo1999@gmail.com>
2021-05-06 21:02:53 +0530

    minor changes

--- static/js/preloader.js
+++ static/js/preloader.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
     setTimeout(function(){
      $("#preloader").delay(650).removeClass("animation").addClass("over");
      $(".pre-overlay").css({"height" : "0%"});
-    }, 4000);
+    }, 1000);
    });
